71cde863ce45fbacc11f8989183013fc62e66066
Brandon Nichols <brandonaronnichols@gamil.com>
2019-06-07 01:32:16 -0500

    Added mouseenter and click event for site on travel images on img-content

--- js/index.js
+++ js/index.js
@@ -3,10 +3,40 @@ let links = document.querySelectorAll('.nav-link');
 
 links.forEach(link =>{
     link.addEventListener('wheel', function(){
-        console.log(this);
         link.style.display = "none";
     });
 });
 
-let travelImg = document.querySelector('.img-content');
+let travelImg = document.querySelectorAll('.img-content img');
+let wholePage = document.querySelector('body');
 
+function pictureMode(pic){
+    let picturePresenter = document.createElement('div');
+    let picture = document.createElement('img');
+    picture.src = pic.src;
+
+    picturePresenter.style.position = "fixed";
+    picturePresenter.style.height = "100%";
+    picturePresenter.style.width = "100%";
+    picturePresenter.style.background = "rgba(0, 0, 0, 0.5)";
+    picturePresenter.style.zIndex = "1";
+
+    picture.style.marginLeft = "700px";
+    picture.style.marginTop = "300px";
+
+    wholePage.prepend(picturePresenter);
+    picturePresenter.appendChild(picture);
+    console.log(picturePresenter);
+
+    picturePresenter.addEventListener('click', function(){
+        wholePage.removeChild(wholePage.firstChild);
+    });
+}
+
+travelImg.forEach(img => {
+    img.addEventListener('mouseenter', function(){
+        pictureMode(this);
+        //this.style.display
+        //this.style.margin = "0 auto";
+    });
+});
