13267e3dbd0d4dfdafffb47fc58ccf5324165fa6
Brandon Nichols <brandonaronnichols@gamil.com>
2019-06-07 00:23:18 -0500

    Added event to make links display none when mousewheel

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+let links = document.querySelectorAll('.nav-link');
+
+links.forEach(link =>{
+    link.addEventListener('wheel', function(){
+        console.log(this);
+        link.style.display = "none";
+    });
+});
+
+let travelImg = document.querySelector('.img-content');
+
